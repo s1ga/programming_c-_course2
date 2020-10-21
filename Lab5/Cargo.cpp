a79de6d5405997e7e30ea14bb7495b5a951abf40
s1ga <s.v.harlanov@gmail.com>
2020-10-21 23:00:39 +0300

    make realization of methods

--- Lab5/Cargo.cpp
+++ Lab5/Cargo.cpp
@@ -2,13 +2,25 @@
 #include<cmath>
 #include"Cargo.h"
 
-Plane::Plane() : minWeight(5000), minDistance(1000) {
+// setting plane params in constructor
+Plane::Plane() : minWeight(10000), minDistance(1000) {
 	this->speed = 900;
-	this->costPerKm = 0.2;
+	this->costPerKm = 0.8;
 };
 
-Train::Train() : minDistance(250)
+// setting train params in constructor
+Train::Train() : minDistance(250) {
+	this->speed = 50;
+	this->costPerKm = 0.07;
+}
 
+// setting car params in constructor
+Car::Car() : maxWeight(15000) {
+	this->speed = 90;
+	this->costPerKm = 0.1;
+}
+
+// set distance of route
 float Cargo::setDistance() {
 	float distance;
 	printf("Input distance of your route: ");
@@ -16,16 +28,36 @@ float Cargo::setDistance() {
 	return distance;
 }
 
+//get speed of transport
 float Cargo::getSpeed() {
 	return this->speed;
 }
 
+// get time of route with 2 signs precision
 float Cargo::getTime(float distance) {
 	float time = distance / this->speed;
 	return round(time * 100) / 100;
 }
 
+// get cost of route with 2 signs precision
 float Cargo::getCost(float distance) {
 	float cost = distance * this->costPerKm;
 	return round(cost * 100) / 100;
 }
+
+//bool Cargo::checkConditions(float minWeight = 0, float maxWeight = 0, float minDistance = 0) {
+//	bool flag = false;
+//
+//	//checking for weight
+//	if (minWeight && maxWeight) {
+//		if (this->min)
+//	}
+//	else {
+//		flag = true;
+//	}
+//
+//	//checking for distance
+//
+//
+//	return flag;
+//}
--- Lab5/Cargo.h
+++ Lab5/Cargo.h
@@ -1,31 +1,34 @@
 class Cargo {
 protected: 
-	float speed;
-	float costPerKm;
+	float speed = 0; //cargo transport speed in km/h
+	float costPerKm = 0; //cargo transport cost for 1 km
 public:
-	float setDistance();
-	float getSpeed();
-	float getTime(float distance);
-	float getCost(float distance);
+	float setDistance(); //setter for distance of route
+	float getSpeed(); // getter for speed of transport
+	float getTime(float distance); // getter for route time
+	float getCost(float distance); // getter for route cost
+	//virtual bool checkConditions(float minWeight, float maxWeight, float minDistance); // checks for transport conditions
 };
 
 class Plane : public Cargo {
 private:
-	float minWeight;
-	float minDistance;
+	float minWeight; // min weight of route for using this transport
+	float minDistance; // min distance of route for using this transport
 public:
-	Plane();
-};
+	Plane(); // default constructor
+}; 
 
 class Train : public Cargo {
 private: 
-	float minDistance;
+	float minDistance; // min distance of route for using this transport
 public:
-	Train();
+	Train(); // default constructor
 };
 
 class Car : public Cargo {
+private:
+	float maxWeight; // max weight of route for using this transport
 public:
-	Car();
+	Car(); // default constructor
 };
 
